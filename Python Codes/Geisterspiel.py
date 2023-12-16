90396ae4d39e5901332637f190db494d36591123
Alexandraa8 <Alexandraaltun@outlook.de>
2023-12-16 19:45:03 +0100

    Zwischenergebnis

---
+++ Python Codes/Geisterspiel.py
@@ -0,0 +1,18 @@
+#Geisterspiel
+
+from random import randint
+
+print("Geisterspiel")
+begruessung = 'Hallo'
+name = input ('Wie heißt du?')
+begruessung = begruessung + ' ' + name + '.'
+print(begruessung)
+
+aufforderung = 'Bist du bereit? Falls du es bist, antworte mit ja!'
+antwort = 'Viel Spaß!'
+aufforderung = aufforderung + ' ' + antwort + ' '
+print(aufforderung)
+
+print("======================")
+
+
