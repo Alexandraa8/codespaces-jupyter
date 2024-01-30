fa7c6c27bd51f48d1b20bb9cd98b8c4a5856e02e
Alexandraa8 <Alexandraaltun@outlook.de>
2024-01-30 20:57:20 +0100

    Fortsetzung Snake 2

--- Python Codes/Snakespiel.py
+++ Python Codes/Snakespiel.py
@@ -20,3 +20,21 @@ def zeichner():
             kopf = False
         else:
             pygame.draw.rect(screen, (47,79,79), (Coords[0], Coords[1],partikel, partikel),0)
+
+go = True
+anhang = None
+apfelInd = -1
+ende = False
+score = 0
+while go:
+    for event in pygame.get():
+        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_UP and richtung !=2:
+                richtung = 0
+            if event.key == pygame.K_RIGHT and richtung != 3:
+                richtung = 1
+            if event.key == pygame.K_DOWN and richtung != 0:
+                richtung = 2
+            if event.key == pygame.K_LEFT and richtung != 1:
+                richtung = 3
