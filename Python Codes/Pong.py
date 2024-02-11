251aa27f35f3e8d958ed56b104fc26f157a0f016
Alexandraa8 <Alexandraaltun@outlook.de>
2024-02-11 17:04:50 +0100

    Pong Game End

--- Python Codes/Pong.py
+++ Python Codes/Pong.py
@@ -65,11 +65,11 @@ def reset():
 
     speed = 0
     bxspeed = random.randint(-2,2)
-    if bxspeed := 0 :
+    if bxspeed == 0 :
         bxspeed = 1
 
     byspeed = random.randint(-2,2)
-    if byspeed := 0 :
+    if byspeed == 0 :
         byspeed = 2  
     
     screen.fill((0,0,0))
@@ -82,4 +82,37 @@ def ballblock():
     global byspeed, bxspeed, leben
     if by-brad <= 0:
         byspeed *= -1
-        
+    if bx-brad <= 0:
+        bxspeed *= -1
+    if bx+brad >= x:
+        bxspeed *= -1
+    if by >= 435 and by <= 440:
+        if bx >= sx-15 and bx <= sx+ sbreite+15:
+            byspeed *= -1
+        else:
+            leben -= 1
+            reset()
+
+def sbewegung():
+    global sx
+    sx += speed
+
+while leben>0:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed = -2
+            if event.key == pygame.K_RIGHT:
+                speed = 2
+    screen.fill((0,0,0))
+    sbewegung()
+    sblock()
+    pygame.draw.rect(screen (255,40,0), (sx, sy, sbreite, shoehe), 0)
+    ballbewegung()
+    ballblock()
+    pygame.draw.circle(screen, (255,255,0), (bx, by), brad, 0)
+    pygame.display.flip()
+    pygame.time.wait(5)
+
+print("verloren!")
